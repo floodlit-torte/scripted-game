660ef6b945922a93c14d9a90344d7f727e211a9e
Alibi <arkeyalibi171007@gmail.com>
2023-03-24 20:03:07 +0600

    particle system added

--- terrain-game/Assets/Scripts/CollisionHandler.cs
+++ terrain-game/Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private ParticleSystem DamageVFX;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        DamageVFX.Play();
     }
 }
